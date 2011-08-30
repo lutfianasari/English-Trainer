01c626d541dcc9813b4fcccbf3223bee6f3f70d6
ariestien lutfianasari <lutefnyew@dhcp-159.i.kyushu-u.ac.jp>
2011-08-30 17:43:36 +0900

    Create basic pages.

--- Classes/EnglishTrainerViewController.h
+++ Classes/EnglishTrainerViewController.h
@@ -9,6 +9,27 @@
 #import <UIKit/UIKit.h>
 
 @interface EnglishTrainerViewController : UIViewController {
+	
+	//IBOutlet UIView for each page
+	IBOutlet UIView *login_view;
+	IBOutlet UIView *register_view;
+	IBOutlet UIView *menu_view;
+	IBOutlet UIView *profile_view;
+	IBOutlet UIView *search_view;
+	IBOutlet UIView *bookmarklist_view;
+	IBOutlet UIView *history_view;
+	IBOutlet UIView *help_view;
+	IBOutlet UIView *resultdetail_view;
+	
+	//IBOutlet for Login
+	
+	//IBAction for Login
+	
+	//IBOutlet for Register
+	
+	//IBAction for Register
+	
+	//...
 
 }
 
